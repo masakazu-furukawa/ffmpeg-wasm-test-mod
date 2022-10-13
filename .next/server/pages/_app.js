f7936af4a832d933f168b7d14b1bc53af50a3eb2
Masakazu Furukawa <m-furukawa@fujitsu.com>
2022-10-13 07:51:41 +0000

    ffmpeg-test

---
+++ .next/server/pages/_app.js
@@ -0,0 +1,178 @@
+"use strict";
+(() => {
+var exports = {};
+exports.id = 888;
+exports.ids = [888];
+exports.modules = {
+
+/***/ 221:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  "default": () => (/* binding */ MyApp)
+});
+
+// EXTERNAL MODULE: external "react/jsx-runtime"
+var jsx_runtime_ = __webpack_require__(997);
+// EXTERNAL MODULE: external "react"
+var external_react_ = __webpack_require__(689);
+;// CONCATENATED MODULE: external "prop-types"
+const external_prop_types_namespaceObject = require("prop-types");
+var external_prop_types_default = /*#__PURE__*/__webpack_require__.n(external_prop_types_namespaceObject);
+;// CONCATENATED MODULE: external "next/head"
+const head_namespaceObject = require("next/head");
+var head_default = /*#__PURE__*/__webpack_require__.n(head_namespaceObject);
+// EXTERNAL MODULE: external "@mui/material/styles"
+var styles_ = __webpack_require__(442);
+;// CONCATENATED MODULE: external "@mui/material/CssBaseline"
+const CssBaseline_namespaceObject = require("@mui/material/CssBaseline");
+var CssBaseline_default = /*#__PURE__*/__webpack_require__.n(CssBaseline_namespaceObject);
+;// CONCATENATED MODULE: external "@emotion/react"
+const react_namespaceObject = require("@emotion/react");
+// EXTERNAL MODULE: ./src/theme.ts + 1 modules
+var theme = __webpack_require__(465);
+// EXTERNAL MODULE: ./src/createEmotionCache.ts + 1 modules
+var createEmotionCache = __webpack_require__(544);
+;// CONCATENATED MODULE: ./pages/_app.tsx
+
+
+
+
+
+
+
+
+
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = (0,createEmotionCache/* default */.Z)();
+function MyApp(props) {
+    const { Component , emotionCache =clientSideEmotionCache , pageProps  } = props;
+    return /*#__PURE__*/ (0,jsx_runtime_.jsxs)(react_namespaceObject.CacheProvider, {
+        value: emotionCache,
+        children: [
+            /*#__PURE__*/ (0,jsx_runtime_.jsxs)((head_default()), {
+                children: [
+                    /*#__PURE__*/ jsx_runtime_.jsx("title", {
+                        children: "My page"
+                    }),
+                    /*#__PURE__*/ jsx_runtime_.jsx("meta", {
+                        name: "viewport",
+                        content: "initial-scale=1, width=device-width"
+                    })
+                ]
+            }),
+            /*#__PURE__*/ (0,jsx_runtime_.jsxs)(styles_.ThemeProvider, {
+                theme: theme/* default */.Z,
+                children: [
+                    /*#__PURE__*/ jsx_runtime_.jsx((CssBaseline_default()), {}),
+                    /*#__PURE__*/ jsx_runtime_.jsx(Component, {
+                        ...pageProps
+                    })
+                ]
+            })
+        ]
+    });
+}
+MyApp.propTypes = {
+    Component: (external_prop_types_default()).elementType.isRequired,
+    emotionCache: (external_prop_types_default()).object,
+    pageProps: (external_prop_types_default()).object.isRequired
+};
+
+
+/***/ }),
+
+/***/ 544:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  "Z": () => (/* binding */ createEmotionCache)
+});
+
+;// CONCATENATED MODULE: external "@emotion/cache"
+const cache_namespaceObject = require("@emotion/cache");
+var cache_default = /*#__PURE__*/__webpack_require__.n(cache_namespaceObject);
+;// CONCATENATED MODULE: ./src/createEmotionCache.ts
+
+function createEmotionCache() {
+    return cache_default()({
+        key: "css"
+    });
+}
+
+
+/***/ }),
+
+/***/ 465:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  "Z": () => (/* binding */ src_theme)
+});
+
+// EXTERNAL MODULE: external "@mui/material/styles"
+var styles_ = __webpack_require__(442);
+;// CONCATENATED MODULE: external "@mui/material/colors"
+const colors_namespaceObject = require("@mui/material/colors");
+;// CONCATENATED MODULE: ./src/theme.ts
+
+
+// Create a theme instance.
+const theme = (0,styles_.createTheme)({
+    palette: {
+        primary: {
+            main: "#556cd6"
+        },
+        secondary: {
+            main: "#19857b"
+        },
+        error: {
+            main: colors_namespaceObject.red.A400
+        }
+    }
+});
+/* harmony default export */ const src_theme = (theme);
+
+
+/***/ }),
+
+/***/ 442:
+/***/ ((module) => {
+
+module.exports = require("@mui/material/styles");
+
+/***/ }),
+
+/***/ 689:
+/***/ ((module) => {
+
+module.exports = require("react");
+
+/***/ }),
+
+/***/ 997:
+/***/ ((module) => {
+
+module.exports = require("react/jsx-runtime");
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = (__webpack_exec__(221));
+module.exports = __webpack_exports__;
+
+})();
